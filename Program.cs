22408735c85f47358503f934ad84b6998c3c232c
Katt <Raxxyn@gmail.com>
2022-12-18 21:56:41 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace examples_for_bunny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloKanin("katt", 5, false);
+            Console.ReadLine();
+        }
+
+        static void HelloKanin(string fluffybuns, int a, bool IsEndTrueOrFalse)
+        {
+            
+            for (int i = 0; i < a; i++)
+            {
+
+                var hmm = IsEndTrueOrFalse ? "!" : "?";
+                Console.WriteLine("Hello " + fluffybuns + hmm);
+                
+            }
+        }
+
+        
+    }
+}
